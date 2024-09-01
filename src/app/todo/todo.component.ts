e88e545f31eb2d988e5462344545479ccd35ec68
ichorville <ichorville@gmail.com>
2024-09-01 22:37:40 +0100

    Indentation issues still not fixed

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -2,13 +2,10 @@ import { Component, inject, ViewChild } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AsyncPipe, NgFor, JsonPipe } from '@angular/common';
 
-import {
-  MatCheckboxChange,
-  MatCheckboxModule,
-} from '@angular/material/checkbox';
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatListModule, MatListOption, MatSelectionList, } from '@angular/material/list';
+import { MatListModule, MatListOption, MatSelectionList } from '@angular/material/list';
 
 import { combineLatest, Observable } from 'rxjs';
 
@@ -39,7 +36,7 @@ export class TodoComponent {
     users: this._todoService.users$,
     currentUser: this._todoService.currentUser$,
     isLoading: this._todoService.isLoading$,
-    isFiltered: this._todoService.isFiltered$
+    isFiltered: this._todoService.isFiltered$,
   });
 
   public onUserSelection(event: MatSelectChange): void {
@@ -53,7 +50,8 @@ export class TodoComponent {
 
   public onToggle(user: User, list: MatSelectionList): void {
     const todos = list._value as unknown as Todo[];
-    todos.forEach(todo => todo.completed = true);
+    todos.forEach((todo) => (todo.completed = true));
     user.filteredTodos = todos;
   }
 }
+
